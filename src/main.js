1560885ca0309c7749aa11b7c4ffcba27d98a607
Anna Huang <annatointro@gmail.com>
2019-08-23 14:43:38 -0600

    page setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import StudentList from './pages/StudentList.vue'
 
 Vue.config.productionTip = false
+Vue.component('StudentList',StudentList)
 
 new Vue({
   render: h => h(App),
